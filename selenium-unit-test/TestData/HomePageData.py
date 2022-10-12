8fc274a0c28bda568dae616f03ac299cb9c5870a
Vamsi <kasyapvamsi363@gmail.com>
2022-10-12 10:23:47 +0530

    Second commit from source

--- selenium-unit-test/TestData/HomePageData.py
+++ selenium-unit-test/TestData/HomePageData.py
@@ -1,4 +1,4 @@
 class HomePageData:
     test_HomePage_Data = [
         {"Email": "hello@gmail.com", "Password": "123456", "Name": "Vamsi_Kasyap", "Gender": "Male", "index": 0},
-        {"Email": "Sai@gmail.com", "Password": "345678", "Name": "Sai_Karthika", "Gender": "Female", "index": 1},{"Email": "nora@gmail.com", "Password": "345678", "Name": "nora", "Gender": "Female", "index": 2},{"Email": "kar@gmail.com", "Password": "345678", "Name": "kar", "Gender": "Female", "index": 3}]
+        {"Email": "Sai@gmail.com", "Password": "345678", "Name": "Sai_Karthika", "Gender": "Female", "index": 1},{"Email": "nora@gmail.com", "Password": "345678", "Name": "nora", "Gender": "Female", "index": 2},{"Email": "kar@gmail.com", "Password": "345678", "Name": "kar", "Gender": "Female", "index": 3},{"Email": "sat@gmail.com", "Password": "123456", "Name": "sat_Kasyap", "Gender": "Male", "index": 4}]
--- selenium-unit-test/TestData/excelDemo.py
+++ selenium-unit-test/TestData/excelDemo.py
@@ -9,7 +9,7 @@ cell = sheet.cell(row=1, column=2)
 
 print(cell.value)
 
-sheet.cell(row=2, column=2).value = 'vamsi'
+sheet.cell(row=2, column=2).value = 'kasyap'
 
 book.save(r"C:\Users\Lenovo\Downloads\pypxl.xlsx")
 
